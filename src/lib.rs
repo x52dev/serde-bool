35d2fe0c6bb4a9aa1f57b1c7063b3b3a7c83426a
Rob Ede <robjtede@icloud.com>
2023-11-30 12:31:04 -0500

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,92 @@
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+/// Type that only deserializes from the `true` boolean value.
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
+pub struct True;
+
+impl<'de> Deserialize<'de> for True {
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        if bool::deserialize(deserializer)? {
+            Ok(Self)
+        } else {
+            Err(serde::de::Error::invalid_value(
+                serde::de::Unexpected::Bool(false),
+                &"the `true` boolean",
+            ))
+        }
+    }
+}
+
+impl Serialize for True {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        serializer.serialize_bool(true)
+    }
+}
+
+/// Type that only deserializes from the `false` boolean value.
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
+pub struct False;
+
+impl<'de> Deserialize<'de> for False {
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        if bool::deserialize(deserializer)? {
+            Err(serde::de::Error::invalid_value(
+                serde::de::Unexpected::Bool(true),
+                &"the `false` boolean",
+            ))
+        } else {
+            Ok(Self)
+        }
+    }
+}
+
+impl Serialize for False {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        serializer.serialize_bool(false)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Debug, Deserialize, PartialEq)]
+    struct Tru {
+        foo: True,
+    }
+
+    #[test]
+    fn de_true() {
+        assert_eq!(
+            Tru { foo: True },
+            serde_json::from_str::<Tru>(r#"{"foo": true}"#).unwrap(),
+        );
+
+        serde_json::from_str::<Tru>(r#"{"foo": false}"#).unwrap_err();
+    }
+
+    #[test]
+    fn se_true() {
+        assert_eq!(
+            Tru { foo: True },
+            serde_json::from_str::<Tru>(r#"{"foo": true}"#).unwrap(),
+        );
+
+        serde_json::from_str::<Tru>(r#"{"foo": false}"#).unwrap_err();
+    }
+
+    #[derive(Debug, Deserialize, PartialEq)]
+    struct Fal {
+        foo: False,
+    }
+
+    #[test]
+    fn de_false() {
+        assert_eq!(
+            Fal { foo: False },
+            serde_json::from_str::<Fal>(r#"{"foo": false}"#).unwrap(),
+        );
+
+        serde_json::from_str::<Fal>(r#"{"foo": true}"#).unwrap_err();
+    }
+}
